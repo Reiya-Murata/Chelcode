c7ef3c8173bcd4354edeaaff505928d3e0bb950d
村田　玲耶 <f.zero39.m@gmail.com>
2023-02-16 22:05:51 +0900

    13th comit

--- page-works.php
+++ page-works.php
@@ -17,26 +17,23 @@ Template Name:works
       <div class="container-5">
         
       <!-- #region -->
-<div class="sliderArea">
-  <div class="regular_3 slider">
     <div class="portfolio">
       <?php echo CFS()->get("work_title_1");?>
       <a href="<?php echo CFS()->get("work_url_1");?>"><img class="work" src="<?php echo CFS()->get("work_img_1");?>" alt="制作サイト1"></a>
     </div>
     <div class="portfolio">
     <?php echo CFS()->get("work_title_2");?>
-      <a href="<?php echo CFS()->get("work_url_2");?>"><img class="work" src="<?php echo CFS()->get("work_img_2");?>/img/スクリーンショット (4).png" alt="制作サイト2"></a>
+      <a href="<?php echo CFS()->get("work_url_2");?>"><img class="work" src="<?php echo CFS()->get("work_img_2");?>></a>
     <div class="portfolio">
     <?php echo CFS()->get("work_title_3");?>
-      <a href="    <?php echo CFS()->get("work_url_3");?>"><img class="work" src="<?php echo CFS()->get("work_img_3");?>/img/kasanari.jpg" alt="株式会社kasanari"></a>
+      <a href="    <?php echo CFS()->get("work_url_3");?>"><img class="work" src="<?php echo CFS()->get("work_img_3");?>"></a>
     </div>
     <div class="portfolio">
     <?php echo CFS()->get("work_title_4");?>
       <a href="<?php echo CFS()->get("work_url_4");?>">
-      <img class="work" src="<?php echo CFS()->get("work_img_4");?>/img/スクリーンショット (3).png" alt="制作サイト3"></a>
+      <img class="work" src="<?php echo CFS()->get("work_img_4");?>"></a>
     </div>
-  </div>
-</div>
+
   </section>
 
 
