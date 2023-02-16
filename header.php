c0423c55bbeb102934150a29f8f433d123b69184
村田　玲耶 <f.zero39.m@gmail.com>
2023-02-16 20:30:47 +0900

    10th comit

--- header.php
+++ header.php
@@ -12,50 +12,6 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@1,400;1,600&display=swap" rel="stylesheet">
 <?php wp_head();?>
-<script>
-  const burger=document.querySelector(".burger");
-const nav=document.querySelector(".nav-links");
-const navLinks=document.querySelectorAll(".nav-links li");
-
-burger.addEventListener("click",()=>{
-  nav.classList.toggle("nav-active");
-
-  navLinks.forEach((link, index) => {
-    if (link.style.animation) {
-      link.style.animation = "";
-    } else {
-      // console.log(index);
-      link.style.animation = `navLinksFade 0.5s ease forwards ${
-        index / 7 + 0.4
-      }s`;
-    }
-  });
-  //burger animataion
-  burger.classList.toggle("toggle");
-});
-// burger menu
-
-window.onload = function(){
-
-setTimeout(()=> {
-//loaderを消す
-const loader = document.querySelector(".loader");
-loader.classList.add("loaded");
-// mainを見せる
-const main = document.querySelector(".main");
-main.style.visibility="visible";
-},1500);
-};
-
-// スライダー
-$(document).on('ready', function() {
-  $(".regular_3").slick({
-    dots: true, // ドットインジケーターの表示
-    infinite: true, // スライドのループを有効にするか
-    slidesToShow: 3, // 表示するスライド数を設定
-    slidesToScroll: 1 // スクロールするスライド数を設定
-  });
-});
 </head>
 
 <body>
@@ -112,5 +68,49 @@ $(document).on('ready', function() {
       </div>
         <?php }else{}?>
 
+<script>
+  const burger=document.querySelector(".burger");
+const nav=document.querySelector(".nav-links");
+const navLinks=document.querySelectorAll(".nav-links li");
+
+burger.addEventListener("click",()=>{
+  nav.classList.toggle("nav-active");
+
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      // console.log(index);
+      link.style.animation = `navLinksFade 0.5s ease forwards ${
+        index / 7 + 0.4
+      }s`;
+    }
+  });
+  //burger animataion
+  burger.classList.toggle("toggle");
+});
+// burger menu
+
+window.onload = function(){
+
+setTimeout(()=> {
+//loaderを消す
+const loader = document.querySelector(".loader");
+loader.classList.add("loaded");
+// mainを見せる
+const main = document.querySelector(".main");
+main.style.visibility="visible";
+},1500);
+};
+
+// スライダー
+$(document).on('ready', function() {
+  $(".regular_3").slick({
+    dots: true, // ドットインジケーターの表示
+    infinite: true, // スライドのループを有効にするか
+    slidesToShow: 3, // 表示するスライド数を設定
+    slidesToScroll: 1 // スクロールするスライド数を設定
+  });
+});
 
     
