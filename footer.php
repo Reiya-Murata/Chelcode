9af7ea6a2d8b41420fb902fce7a69d3c2ccc8609
村田　玲耶 <f.zero39.m@gmail.com>
2023-02-16 20:13:21 +0900

    8th comit

--- footer.php
+++ footer.php
@@ -2,7 +2,51 @@
 <div class="copyright"><p>&copy2023 All right reserved</p></div>
 </footer>
 
-<script src="<?php bloginfo("template_url");?>/script.js" ></script>
+<script>
+  const burger=document.querySelector(".burger");
+const nav=document.querySelector(".nav-links");
+const navLinks=document.querySelectorAll(".nav-links li");
+
+burger.addEventListener("click",()=>{
+  nav.classList.toggle("nav-active");
+
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      // console.log(index);
+      link.style.animation = `navLinksFade 0.5s ease forwards ${
+        index / 7 + 0.4
+      }s`;
+    }
+  });
+  //burger animataion
+  burger.classList.toggle("toggle");
+});
+// burger menu
+
+window.onload = function(){
+
+setTimeout(()=> {
+//loaderを消す
+const loader = document.querySelector(".loader");
+loader.classList.add("loaded");
+// mainを見せる
+const main = document.querySelector(".main");
+main.style.visibility="visible";
+},1500);
+};
+
+// スライダー
+$(document).on('ready', function() {
+  $(".regular_3").slick({
+    dots: true, // ドットインジケーターの表示
+    infinite: true, // スライドのループを有効にするか
+    slidesToShow: 3, // 表示するスライド数を設定
+    slidesToScroll: 1 // スクロールするスライド数を設定
+  });
+});
+</script>
 <script src="https://unpkg.com/scrollreveal"></script>
 <script>
 ScrollReveal({ 
