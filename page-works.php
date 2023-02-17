fa0656cc6be816b2010b0b254da392b27d45bac3
村田　玲耶 <f.zero39.m@gmail.com>
2023-02-17 20:11:41 +0900

    works

--- page-works.php
+++ page-works.php
@@ -20,17 +20,18 @@ Template Name:works
       <!-- #region -->
     <div class="portfolio">
       <?php echo CFS()->get("work_title_1");?>
-      <img class="work" src="<?php echo CFS()->get("work_img_1");?>"><?php echo CFS()->get("work_url_1");?>
+      <img class="work" src="<?php echo CFS()->get("work_img_1");?>">
+      <br><?php echo CFS()->get("work_url_1");?>
     </div>
     <div class="portfolio">
     <?php echo CFS()->get("work_title_2");?>
       <img class="work" src="<?php echo CFS()->get("work_img_2");?>">
-      <?php echo CFS()->get("work_url_2");?>
+      <br><?php echo CFS()->get("work_url_2");?>
     </div>
     <div class="portfolio">
     <?php echo CFS()->get("work_title_3");?>
       <img class="work" src="<?php echo CFS()->get("work_img_3");?>">
-      <?php echo CFS()->get("work_url_3");?>
+      <br><?php echo CFS()->get("work_url_3");?>
     </div>
   </div>
   
