0f16ce7336d204dacb39618b7948bfb816d6fb36
村田　玲耶 <f.zero39.m@gmail.com>
2023-02-17 17:51:17 +0900

    お知らせ　入れ替え

--- front-page.php
+++ front-page.php
@@ -24,35 +24,6 @@
           <br><h2>お気軽にお問い合わせください!!</h2>
           <button type="button"><a href="#contact">お問い合わせはこちら➡</a>
           </button>
-          <!-- 制作実績 -->
-          <section class="work_list">
-            <div class="head">制作実績</div>
-            <div class="news_list">
-              <?php
-              $args=array(
-                "post_type"=>"works",//投稿タイプスラッグ
-                "post_per_page"=>3
-              );
-              $the_query=new WP_Query($args);
-              if($the_query->have_posts()):
-              ?>
-              <ul>
-                <?php while($the_query->have_posts()):$the_query->the_post();?>
-                <li>
-                <a href="<?php the_parmalink();?>">
-                <div class="img"><img src="<?php echo CFS()->get("thumbnail");?>"></div>
-                <div class="client">クライアント:<?php echo CFS()->get("client");?></div>
-                </a>
-                </li>
-                <?php endwhile;?>
-            </ul>
-              <?php wp_reset_postdata();?>
-              <?php else:?>
-              <?php endif;?>
-              <div class="btn_stn">
-                <a href="<?php bloginfo('url');?>/works">一覧を見る</a></div>
-            </div>
-          </section>
 
           <!-- お知らせ -->
           <section class="front_news">
@@ -81,4 +52,35 @@
       </div>
     </div>
   </section>
+
+            <!-- 制作実績 -->
+            <section class="work_list">
+            <div class="head">制作実績</div>
+            <div class="news_list">
+              <?php
+              $args=array(
+                "post_type"=>"works",//投稿タイプスラッグ
+                "post_per_page"=>3
+              );
+              $the_query=new WP_Query($args);
+              if($the_query->have_posts()):
+              ?>
+              <ul>
+                <?php while($the_query->have_posts()):$the_query->the_post();?>
+                <li>
+                <a href="<?php the_parmalink();?>">
+                <div class="img"><img src="<?php echo CFS()->get("thumbnail");?>"></div>
+                <div class="client">クライアント:<?php echo CFS()->get("client");?></div>
+                </a>
+                </li>
+                <?php endwhile;?>
+            </ul>
+              <?php wp_reset_postdata();?>
+              <?php else:?>
+              <?php endif;?>
+              <div class="btn_stn">
+                <a href="<?php bloginfo('url');?>/works">一覧を見る</a></div>
+            </div>
+          </section>
+
   <?php get_footer();?>
