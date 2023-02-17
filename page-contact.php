2943c6ba2ec209cf9a938967537738ef9dc19d57
村田　玲耶 <f.zero39.m@gmail.com>
2023-02-18 01:10:48 +0900

    form

--- page-contact.php
+++ page-contact.php
@@ -1,16 +1,34 @@
 <?php get_header();?>
 
 <div id="content" class="page_wrap w_inner" >
+<section class="contact" id="contact">
+  
+  <div class="space-2">
+    <h1>CONTACT</h1>
+    <div class="contactform">
+    <form action="https://api.staticforms.xyz/submit" method="post">
+      <p class="title">お問い合わせ</p>
+      <div class="item">
+        <label class="label">お名前</label>
+        <input type="text" class="inputs" name="name" required>
+      </div>
+      <div class="item">
+        <label class="label">メールアドレス</label>
+        <input type="text" class="inputs" name="email" required>
+      </div>
+      <div class="item">
+        <label class="label">メッセージ</label>
+        <textarea name="message" id="" cols="30" rows="10"></textarea>
+      </div>
+      <div class="button-area">
+        <input type="submit" value="送信">
+      </div>
+      <input type="hidden" name="accessKey" value="afbd65b6-7a6b-48a9-9346-05873694ddd1">
+    </form>
+  </div>
+  </div>
+  <div class="space"></div>
+</section>
 
-<div id="main">
-<?php if (have_posts()):while(have_posts()):the_post();?>
-
-<h1 class="center"><?php the_title();?></h1>
-<div class="page_container">
-  <?php the_content();?>
-</div>
-<?php endwhile;endif;?>
-
-</div>    
 
 <?php get_footer();?>
