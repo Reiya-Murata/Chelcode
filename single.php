1cc3d92c0c1da2793bd7771f5f2eeea8b4336ac2
村田　玲耶 <f.zero39.m@gmail.com>
2023-02-17 22:15:34 +0900

    w_inner修正

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
     <?php if (have_posts()):while(have_posts()):the_post();?>
 
       <h1 class="ttl"><?php the_title();?></h1>
-      <div class="dete"><?php echo get_the_date("Y.m.d");?>|<php the_category(",")?> </div>
+      <div class="dete"><?php echo get_the_date("Y.m.d");?><php the_category(",")?> </div>
       <?php the_content();?>
       
       <?php endwhile;endif;?>
